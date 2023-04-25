bebb22b54d379ba68cef8bd5dcc49cd99ba16601
SanjanaFarahChowdhury <114453139+SanjanaFarahChowdhury@users.noreply.github.com>
2023-04-25 15:06:13 +1000

    Add files via upload

---
+++ MultipleLinearRegression1.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import math
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+# Read dataset into a DataFrame
+df = pd.read_csv("boston.csv")
+
+"""
+BUILD AND EVALUATE A LINEAR REGRESSION MODEL
+"""
+
+# Separate explanatory variables (x) from the response variable (y)
+x = df.iloc[:,:-1].values
+y = df.iloc[:,-1].values
+
+# Split dataset into 60% training and 40% test sets 
+# Note: other % split can be used.
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+
+# Build a linear regression model
+model = LinearRegression()
+
+# Train (fit) the linear regression model using the training set
+model.fit(X_train, y_train)
+
+# Print the intercept and coefficient learned by the linear regression model
+print("Intercept: ", model.intercept_)
+print("Coefficient: ", model.coef_)
+
+# Use linear regression to predict the values of (y) in the test set
+# based on the values of x in the test set
+y_pred = model.predict(X_test)
+
+# Optional: Show the predicted values of (y) next to the actual values of (y)
+df_pred = pd.DataFrame({"Actual": y_test, "Predicted": y_pred})
+print(df_pred)
+
+# Compute standard performance metrics of the linear regression:
+
+# Mean Absolute Error
+mae = metrics.mean_absolute_error(y_test, y_pred)
+# Mean Squared Error
+mse = metrics.mean_squared_error(y_test, y_pred)
+# Root Mean Square Error
+rmse =  math.sqrt(metrics.mean_squared_error(y_test, y_pred))
+# Normalised Root Mean Square Error
+y_max = y.max()
+y_min = y.min()
+rmse_norm = rmse / (y_max - y_min)
+
+# R-Squared
+r_2 = metrics.r2_score(y_test, y_pred)
+
+print("MLR performance:")
+print("MAE: ", mae)
+print("MSE: ", mse)
+print("RMSE: ", rmse)
+print("RMSE (Normalised): ", rmse_norm)
+print("R^2: ", r_2)
+
+
+
+"""
+COMPARE THE PERFORMANCE OF THE LINEAR REGRESSION MODEL
+VS.
+A DUMMY MODEL (BASELINE) THAT USES MEAN AS THE BASIS OF ITS PREDICTION
+"""
+
+# Compute mean of values in (y) test set
+y_base = np.mean(y_test)
+
+# Replicate the mean values as many times as there are values in the test set
+y_pred_base = [y_base] * len(y_test)
+
+
+# Optional: Show the predicted values of (y) next to the actual values of (y)
+df_base_pred = pd.DataFrame({"Actual": y_test, "Predicted": y_pred_base})
+print(df_base_pred)
+
+# Compute standard performance metrics of the baseline model:
+
+# Mean Absolute Error
+mae = metrics.mean_absolute_error(y_test, y_pred_base)
+# Mean Squared Error
+mse = metrics.mean_squared_error(y_test, y_pred_base)
+# Root Mean Square Error
+rmse =  math.sqrt(metrics.mean_squared_error(y_test, y_pred_base))
+
+# Normalised Root Mean Square Error
+y_max = y.max()
+y_min = y.min()
+rmse_norm = rmse / (y_max - y_min)
+
+# R-Squared
+r_2 = metrics.r2_score(y_test, y_pred_base)
+
+print("Baseline performance:")
+print("MAE: ", mae)
+print("MSE: ", mse)
+print("RMSE: ", rmse)
+print("RMSE (Normalised): ", rmse_norm)
+print("R^2: ", r_2)
